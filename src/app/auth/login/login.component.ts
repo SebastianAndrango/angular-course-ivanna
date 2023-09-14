8a1166ff15f6e375ba894c0028aebd76982c52d2
Ismael Sebastián Andrango Peralbo <51093008+SebastianAndrango@users.noreply.github.com>
2023-09-13 21:53:55 -0500

    Create designe and functional login with validators

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  constructor(private formBuilder: FormBuilder, private router: Router) {}
 
+  loginForm = this.formBuilder.group({
+    email: ['isandrango@espe.edu.ec', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  });
+
+  ngOnInit(): void {}
+
+  get email(){
+    return this.loginForm.controls.email;
+  }
+
+  get password(){
+    return this.loginForm.controls.password;
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      console.log('Llamar al servicio de login');
+      this.router.navigateByUrl('/inicio');
+      this.loginForm.reset();
+    } else {
+      this.loginForm.markAllAsTouched();
+      alert('Error al ingresar los datos');
+    }
+  }
 }
